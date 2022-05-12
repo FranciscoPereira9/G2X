9e4ff4cc51736fcaa77d746776554888754779d8
FranciscoPereira9 <francisco.pereira.sjm@gmail.com>
2022-05-12 15:34:36 +0200

    The start of something beautiful

---
+++ imdb-word/imdb_dataset.py
@@ -0,0 +1,219 @@
+import random
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+from torch_geometric.data import Dataset as PyG_Dataset
+from torch_geometric.loader import DataLoader as PyGDataLoader
+from keras.preprocessing import sequence
+from keras.datasets import imdb
+import os
+
+try:
+    import cPickle as pkl
+except:
+    import pickle as pkl
+import utils
+
+# Reproducibility:
+torch.manual_seed(10086)
+torch.cuda.manual_seed(1)
+np.random.seed(10086)
+random.seed(10086)
+# Set parameters:
+max_features = 5000
+maxlen = 400
+
+class IMDB_SentimentDataset(Dataset):
+
+    def __init__(self, data_path="data", setting="train", split_val=0.05, model="original"):
+        if 'id_to_word.pkl' not in os.listdir(data_path):
+            # Load data from original dataset
+            (x_train, y_train), (x_val, y_val) = imdb.load_data(num_words=max_features, index_from=3)
+            word_to_id = imdb.get_word_index()
+            word_to_id = {k: (v + 3) for k, v in word_to_id.items()}
+            word_to_id["<PAD>"] = 0
+            word_to_id["<START>"] = 1
+            word_to_id["<UNK>"] = 2
+            id_to_word = {value: key for key, value in word_to_id.items()}
+            # Pad Reviews
+            x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
+            x_val = sequence.pad_sequences(x_val, maxlen=maxlen)
+            y_train = np.eye(2)[y_train]
+            y_val = np.eye(2)[y_val]
+            # Save Data
+            np.save(data_path+'/x_train.npy', x_train)
+            np.save(data_path+'/y_train.npy', y_train)
+            np.save(data_path+'/x_val.npy', x_val)
+            np.save(data_path+'/y_val.npy', y_val)
+            with open(data_path+'/id_to_word.pkl', 'wb') as f:
+                pkl.dump(id_to_word, f)
+        assert (setting == "train" or setting == "val" or setting == "test"), \
+            "Parameter <setting> must be 'train', 'val' or 'test'."
+        if setting == "train":
+            self.data = np.load(data_path + '/x_train.npy')
+            self.targets = np.load(data_path + '/y_train.npy')
+            split = int(len(self.data)*(1-split_val))
+            self.data = self.data[:split]
+            self.targets = self.targets[:split]
+        elif setting == "val":
+            self.data = np.load(data_path + '/x_train.npy')
+            self.targets = np.load(data_path + '/y_train.npy')
+            split = int(len(self.data) * (1-split_val))
+            self.data = self.data[split:]
+            self.targets = self.targets[split:]
+        else:
+            self.data = np.load(data_path + '/x_val.npy')
+            self.targets = np.load(data_path + '/y_val.npy')
+
+        if model=="l2x":
+            if setting=="train":
+                assert 'pred_train.npy' in os.listdir(data_path),\
+                    "File 'pred_train.npy' is not in the 'data' folder. Generate prediction first. "
+                self.targets = np.load('data/pred_train.npy')
+            if setting == "test":
+                assert 'pred_test.npy' in os.listdir(data_path), \
+                    "File 'pred_train.npy' is not in the 'data' folder. Generate prediction first. "
+                self.targets = np.load('data/pred_test.npy')
+
+        with open(data_path + '/id_to_word.pkl', 'rb') as f:
+            self.id_to_word = pkl.load(f)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        input_ids = self.data[idx]
+        target = self.targets[idx]
+        return {
+            "input_ids": torch.tensor(input_ids, dtype=torch.long),
+            "target": torch.tensor(target, dtype=torch.float)
+        }
+
+    def get_text(self, idx):
+        sentence = []
+        for num in self.data[idx]:
+            if num == 0:
+                sentence.append("-")
+            else:
+                sentence.append(self.id_to_word[num])
+        return " ".join(sentence)
+
+
+
+class IMDB_Graph_SentimentDataset(PyG_Dataset):
+    def __init__(self, root, setting, transform=None, pre_transform=None, pre_filter=None):
+        """
+        Args:
+            root: where the dataset should be stored. This folder is split into raw_dir (downloaded dataset) and
+                  processed_dir (processed dataset).
+            transform: transformations to be used for data augmentation.
+            pre_transform: pre-processing transformations.
+            pre_filter: pre_selection of data.
+        """
+        assert setting in ["train", "test"], "setting must be either 'train' or 'test'."
+        self.setting = setting
+        super(IMDB_Graph_SentimentDataset, self).__init__(root, transform, pre_transform, pre_filter)
+
+    @property
+    def raw_file_names(self):
+        """
+        If the files exists in the raw_dir, the download method is not triggered. Otherwise it calls download method.
+        """
+        return ['x_train.npy', 'x_val.npy', 'y_train.npy', 'y_val.npy', 'id_to_word.pkl']
+
+    @property
+    def processed_file_names(self):
+        """
+        If the files exists in the processed_dir, the process method is not triggered. Otherwise it calls process method.
+        """
+        files = os.listdir(os.path.join(self.processed_dir, self.setting))
+        if len(files) == 0:
+            out = ['non_existing_file.pt']
+        else:
+            out = [os.path.join(self.setting, file) for file in files]
+        return out
+
+    def download(self):
+        # Download to `self.raw_dir`. path = download_url(url, self.raw_dir)
+        # Not being used.
+        pass
+
+    def process(self):
+        if 'id_to_word.pkl' not in os.listdir(self.raw_dir):
+            # Load data from original dataset
+            (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features, index_from=3)
+            word_to_id = imdb.get_word_index()
+            word_to_id = {k: (v + 3) for k, v in word_to_id.items()}
+            word_to_id["<PAD>"] = 0
+            word_to_id["<START>"] = 1
+            word_to_id["<UNK>"] = 2
+            self.word_to_id = word_to_id
+            id_to_word = {value: key for key, value in word_to_id.items()}
+            self.id_to_word = id_to_word
+            # Pad Reviews
+            x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
+            x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
+            y_train = np.eye(2)[y_train]
+            y_test = np.eye(2)[y_test]
+            # Save Data
+            np.save(self.raw_dir + '/x_train.npy', x_train)
+            np.save(self.raw_dir + '/y_train.npy', y_train)
+            np.save(self.raw_dir + '/x_val.npy', x_test)
+            np.save(self.raw_dir + '/y_val.npy', y_test)
+            with open(self.raw_dir + '/id_to_word.pkl', 'wb') as f:
+                pkl.dump(self.id_to_word, f)
+        else:
+            x_train = np.load(self.raw_dir + '/x_train.npy')
+            y_train = np.load(self.raw_dir + '/y_train.npy')
+            x_test = np.load(self.raw_dir + '/x_val.npy')
+            y_test = np.load(self.raw_dir + '/y_val.npy')
+            with open(self.raw_dir + '/id_to_word.pkl', 'rb') as f:
+                self.id_to_word = pkl.load(f)
+        # Load Glove embeddings
+        glove = utils.load_glove_model()
+        if self.setting == 'train':
+            # Loop through train documents
+            idx = 0
+            for i, doc in enumerate(x_train):
+                # Build Graph Structure, Node Features, Edge Indices and Edge Features
+                graph = utils.build_graph(doc, y_train[i], self.id_to_word, glove)
+                # Save Data
+                torch.save(graph, os.path.join(self.processed_dir + '/train', f'data_{idx}.pt'))
+                idx += 1
+                if idx == 500:
+                    break
+            print("Train data was parsed and saved successfully.")
+        elif self.setting == 'test':
+            # Loop through test documents
+            idx = 0
+            for i, doc in enumerate(x_test):
+                # Build Graph Structure, Node Features, Edge Indices and Edge Features
+                graph = utils.build_graph(doc, y_test[i], self.id_to_word, glove)
+                # Save Data
+                torch.save(graph, os.path.join(self.processed_dir + '/test', f'data_{idx}.pt'))
+                idx += 1
+                if idx == 500:
+                    break
+            print("Test data was parsed and saved successfully.")
+        return True
+
+    def len(self):
+        return len(self.processed_file_names)
+
+    def get(self, idx):
+        folder = os.path.join(self.processed_dir, self.setting)
+        data = torch.load(os.path.join(folder, f'data_{idx}.pt'))
+        return {"input": data, "target": data.y}
+
+
+if __name__ == "__main__":
+    training_data = IMDB_Graph_SentimentDataset(root="data", setting="train")
+    test_data = IMDB_Graph_SentimentDataset(root="data", setting="test")
+    train_loader = PyGDataLoader(training_data, batch_size=2)
+    test_loader = PyGDataLoader(test_data, batch_size=2)
+    for step, data in enumerate(train_loader):
+        print('=======')
+        print(f'Step {step + 1}:')
+        print(data)
+        print()
+    print("Wait...")
---
+++ imdb-word/utils.py
@@ -0,0 +1,172 @@
+import numpy as np
+import torch
+from torch_geometric.data import Data
+import networkx as nx
+import matplotlib.pyplot as plt
+import os
+
+
+def get_selected_words(x_single, score, id_to_word, k):
+    selected_words = {}  # {location: word_id}
+
+    selected = np.argsort(score)[-k:]
+    selected_k_hot = np.zeros(400)
+    selected_k_hot[selected] = 1.0
+
+    x_selected = (x_single * selected_k_hot).astype(int)
+    return x_selected
+
+
+def create_dataset_from_score(dataset, scores, k):
+    id_to_word = dataset.id_to_word
+    x = dataset.data
+    new_data = []
+    new_texts = []
+    for i, x_single in enumerate(x):
+        x_selected = get_selected_words(x_single,
+                                        scores[i], id_to_word, k)
+
+        new_data.append(x_selected)
+
+    np.save('data/x_val-L2X.npy', np.array(new_data))
+
+
+def calculate_acc(pred, y):
+    return np.mean(np.argmax(pred, axis=1) == np.argmax(y, axis=1))
+
+
+def preprocess_document(document, sentence_spliter='.', word_spliter=' ', punct_mark=','):
+    # lowercase all words and remove trailing whitespaces
+    document = document.lower().strip()
+
+    # remove unwanted punctuation marks
+    for pm in punct_mark:
+        document = document.replace(pm, '')
+
+    # get list of sentences which are non-empty
+    sentences = [sent for sent in document.split(sentence_spliter) if sent != '']
+
+    # get list of sentences which are lists of words
+    document = []
+    for sent in sentences:
+        words = sent.strip().split(word_spliter)
+        document.append(words)
+
+    return document
+
+
+def get_entities(document, id_to_word, pre_trained_emb):
+    """
+    Args:
+        document: a list of enconded words in a document -> [0, 0, 0, 50, 29, 500, 20, ...]
+    Returns:
+        node_features: node feature matrix with shape [#nodes,#features]
+        unique_words: encoded words that correspond to the nodes -> node_features[109] corresponds to unique_word[109]
+    """
+
+    # in our case, node entities are all the unique words
+    node_features = []
+    unique_words = []
+    for i in range(len(document)):
+        if document[i] not in unique_words and document[i] not in [0, 1]:
+            # Append node features to list -> pre-trained embedding
+            word = id_to_word[document[i]]
+            if word in pre_trained_emb.keys():
+                embedding_matrix = pre_trained_emb[word].tolist()
+            else:
+                embedding_matrix = (2 * np.random.random_sample(50) - 1).tolist() # randomly initialized uniform embedding [-1,1]
+            node_features.append(embedding_matrix)
+            # Track document words
+            unique_words.append(document[i])
+    return torch.tensor(node_features, dtype=torch.float), torch.tensor(unique_words, dtype=torch.int)
+
+
+def get_relations(document, p=2):
+    """
+    Args:
+        document: a list of encoded words in a document -> [0, 0, 0, 50, 29, 500, 20, ...]
+        p: neighbourhood size to consider
+    Returns:
+        relations: graph connectivity in COO format with shape [2, num_edges]
+    """
+    # relate each node to neighbour p neighbours
+    unique_words = {}
+    node_index = 0
+    for i in range(len(document)):
+        if document[i] not in unique_words.keys() and document[i] not in [0, 1]:
+            # Track document words
+            unique_words[document[i]] = node_index
+            node_index += 1
+
+    relations = []
+    for i in range(len(document) - 1):
+        if document[i] not in [0, 1]:
+            # for every word and the next ones in the sentence
+            for j in range(1, p + 1):
+                if (i + j) < len(document):
+                    # Append both relation in both directions
+                    pair1 = [unique_words[document[i]], unique_words[document[i+j]]]
+                    pair2 = [unique_words[document[i+j]], unique_words[document[i]]]
+                    # only add unique bigrams
+                    if pair1 not in relations:
+                        relations.append(pair1)
+                    if pair2 not in relations:
+                        relations.append(pair2)
+    return torch.tensor(relations, dtype=torch.long).t().contiguous()
+
+
+def build_graph(doc, y, id_to_word, pre_trained_emb, preprocess=False):
+    # preprocess document for standardization
+    if preprocess:
+        doc = preprocess_document(doc)
+
+    # get graph nodes
+    node_features, unique_words = get_entities(doc, id_to_word, pre_trained_emb)
+
+    # get graph edges
+    edges = get_relations(doc)
+
+    # label to tensor
+    y = torch.tensor(np.argmax(y), dtype=torch.long)
+
+    # Create PyTorch Geometric Data object
+    G = Data(x=node_features, edge_index=edges, y=y, node_labels=unique_words)
+
+    return G
+
+
+def load_glove_model(dir="glove"):
+    print("Loading Glove pre-trained embeddings...")
+    glove_model = {}
+    with open(os.path.join(dir, "glove.6B.50d.txt"), 'r', encoding='utf-8') as f:
+        for line in f:
+            split_line = line.split()
+            word = split_line[0]
+            embedding = np.array(split_line[1:], dtype=np.float64)
+            glove_model[word] = embedding
+    print(f"{len(glove_model)} words loaded!")
+    return glove_model
+
+
+def plot_graph(G, title=None):
+    # Need pygraphviz to run this
+    # set figure size
+    plt.figure(figsize=(10, 10))
+
+    # define position of nodes in figure
+    pos = nx.nx_agraph.graphviz_layout(G)
+
+    # draw nodes and edges
+    nx.draw(G, pos=pos, with_labels=True)
+
+    # get edge labels (if any)
+    edge_labels = nx.get_edge_attributes(G, 'weight')
+
+    # draw edge labels (if any)
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+
+    # plot the title (if any)
+    plt.title(title)
+
+    plt.show()
+    return G
